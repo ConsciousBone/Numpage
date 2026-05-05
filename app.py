a04b5143b8e610d6f370f0f9bbd45b0f79fa64b2
Evan <68429595+ConsciousBone@users.noreply.github.com>
2026-05-05 19:43:42 +0100

    Add basic logic for the button stuff
    Thankfully thats all the JS i should need, now its just a matter of arranging the buttons using HTML/CSS and handling stuff in Python

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template, abort
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+@app.route("/button", methods=["POST"])
+def handle_button():
+    data = request.get_json()
+    button_id = data.get("id")
+    print(f"a button was pressed somewhere maybe with id {button_id}")
+    if button_id == "0":
+        print("wow it works!")
+    return jsonify({"message":"success?"})
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html", error = error), 404
